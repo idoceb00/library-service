4386a25a33af28c0801c76be14473d70daee56a9
idoceb00 <idoceb00@estudiantes.unileon.es>
2025-11-30 11:10:48 +0100

    chore: add Air hot-reload configuration for development

--- internal/config/env.go
+++ internal/config/env.go
@@ -3,6 +3,8 @@ package config
 import (
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -15,12 +17,22 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
+	if _, err := os.Stat(".env"); err == nil {
+		if err := godotenv.Load(); err != nil {
+			log.Fatalf("Error loading .env file")
+		} else {
+			log.Println("Loaded .env file")
+		}
+	} else {
+		log.Println("No .env file found; assuming database is already connected")
+	}
+
 	cfg := &Config{
-		DBHost:     os.Getenv("DB_HOST"),
-		DBUser:     os.Getenv("DB_USER"),
-		DBPassword: os.Getenv("DB_PASSWORD"),
-		DBName:     os.Getenv("DB_NAME"),
-		DBPort:     os.Getenv("DB_PORT"),
+		DBHost:     os.Getenv("PGHOST"),
+		DBUser:     os.Getenv("PGUSER"),
+		DBPassword: os.Getenv("PGPASSWORD"),
+		DBName:     os.Getenv("PGDATABASE"),
+		DBPort:     os.Getenv("PGPORT"),
 		APIPort:    os.Getenv("API_PORT"),
 	}
 
